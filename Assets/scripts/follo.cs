8dba6dad27425ded9dac08a8e62f7b448166ea49
Isa Deu <ipd220@nyu.edu>
2018-02-19 17:12:47 -0500

    enemy AI implemented n jumps kinda (pantalla final)

---
+++ Assets/scripts/follo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class follo : MonoBehaviour
+{
+
+	public Transform Player;
+	public int MoveSpeed = 4;
+	public int MaxDist = 10;
+	public int MinDist = 5;
+
+
+
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		transform.LookAt(Player);
+
+		if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+		{
+
+			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+			if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+			{
+				SceneManager.LoadScene ("GO");
+				//Here Call any function U want Like Shoot at here or something
+			}
+
+		}
+	}
+}
---
+++ Assets/scripts/mov.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mov : MonoBehaviour {
+
+	public GameObject bun;
+	public float rotSpeed = 230.0f;
+	public float forSpeed = 100f;
+	public float jumpspeed = 8f;
+	public bool isitgoing = false;
+	public GameObject player;
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * rotSpeed; //150  rotation
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * forSpeed; //3
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Debug.Log ("spacing");
+			GetComponent<Rigidbody> ().velocity = Vector3.up * jumpspeed;
+			Physics.gravity = new Vector3 (0,-10,0);
+
+			//GameObject.rigidbody. AddForce(Vector3.up * jumphight)
+
+			//transform.Translate (Time.deltaTime * 30f);
+		}
+
+
+	}
+}
