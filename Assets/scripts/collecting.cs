4813d26e1eb7252dfe92670b1e942bb6f5c6eec5
Isa Deu <ipd220@nyu.edu>
2018-02-19 18:30:12 -0500

    snacks on destroy

---
+++ Assets/scripts/collecting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collecting : MonoBehaviour {
+	public int count; 
+
+	// Use this for initialization
+	void Start () {
+		count = 0; 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+
+	void OnTriggerEnter (Collider c) {
+		if (c.gameObject.CompareTag ("Player")) {
+			Debug.Log ("oy nice food");
+			Destroy(gameObject, 1);
+		}
+	
+	
+	}
+}
