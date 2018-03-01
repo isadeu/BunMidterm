78c4f9f5b0b3d937fd281ff94959c6ee13c332e7
Isa Deu <ipd220@nyu.edu>
2018-02-28 20:50:57 -0500

    carrot model and carrot rotation

---
+++ Assets/scripts/rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+	public float rotationspeed = 40;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("why isnt this printing");
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * ( Time.deltaTime * rotationspeed), Space.World);
+		
+	}
+}
